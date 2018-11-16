8bbb3518b95069766000b4d67337f9532b84c18f
fbjoker <39256912@qq.com>
2018-11-16 10:46:04 +0800

    Initial commit

---
+++ src/main/java/com/alex/hiveutf/HiveUdf.java
@@ -0,0 +1,9 @@
+package com.alex.hiveutf;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+public class HiveUdf extends UDF {
+    public static void main(String[] args) {
+        System.out.println("xx");
+    }
+}
