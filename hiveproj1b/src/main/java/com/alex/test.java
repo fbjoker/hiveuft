cf3f936f781424f9a4723f448e55b125f6b0a1a7
fbjoker <39256912@qq.com>
2018-11-19 20:57:41 +0800

    1119

---
+++ hiveproj1b/src/main/java/com/alex/test.java
@@ -0,0 +1,53 @@
+package com.alex;
+
+public class test {
+
+    public static void main(String[] args) {
+
+        String a = "lsadjf ";
+
+        System.out.println();
+        System.out.println(add(33, 1));
+        String s = new String();
+
+    }
+
+
+    public static int add(int aacc, int b) {
+        return aacc + b;
+    
+    }
+
+}
+class stu {
+    public stu() {
+    }
+
+    class emp {
+
+        int age;
+        int name;
+
+        public emp(int age, int name) {
+            this.age = age;
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public int getName() {
+            return name;
+        }
+
+        public void setName(int name) {
+            this.name = name;
+        }
+    }
+
+}
