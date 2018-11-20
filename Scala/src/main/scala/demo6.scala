1fa472a585f912facef0efaa8ce84e479fba3d4e
fbjoker <39256912@qq.com>
2018-11-20 21:00:02 +0800

    1120

--- Scala/src/main/scala/demo6.scala
+++ Scala/src/main/scala/demo6.scala
@@ -1,18 +1,18 @@
 object demo6 {
 
   def main(args: Array[String]): Unit = {
-    val a = List(1,2,3)
-    val b = List(55,32,99)
+    val a = List(1, 2, 3)
+    val b = List(55, 32, 99)
 
     val map = Map("alxe" -> a, "boom" -> b)
     val alex = map.get("alxe")
-    val map2=scala.collection.mutable.Map("alex"->1,"bin"->2)
+    val map2 = scala.collection.mutable.Map("alex" -> 1, "bin" -> 2)
 
     println(alex.get(1))
     println(alex)
     println(a(1))
 
-    map2 += ("kaka"->999)
+    map2 += ("kaka" -> 999)
 
     println(map2)
     for (elem <- map2) {
@@ -32,5 +32,5 @@ object demo6 {
     for ((k, v) <- map2) println(k + " is mapped to " + v)
 
 
-
+  }
 }
--- Scala/src/main/scala/scalademo3.scala
+++ Scala/src/main/scala/scalademo3.scala
@@ -1,3 +1,5 @@
+import scala.util.control.Breaks
+
 object scalademo3 {
 
   def main(args: Array[String]): Unit = {
@@ -11,6 +13,19 @@ object scalademo3 {
 
     val for5 = for(i <- 1 to 10) yield i
     println(for5)
+      val breaks = new Breaks
+      var flag=1
+    breaks.breakable{
+      while (flag<10){
+        flag+=1;
+
+        if(flag==6){
+          breaks.break()
+        }
+        println(flag)
+
+      }
+    }
 
 
 
