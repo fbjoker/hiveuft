6d9fa91674833ffc8a95713e9c9314cb809355e0
fbjoker <39256912@qq.com>
2018-11-22 11:33:52 +0800

    1122

--- Scala/src/main/scala/demo8.scala
+++ Scala/src/main/scala/demo8.scala
@@ -5,7 +5,20 @@ object demo8 {
   def main(args: Array[String]): Unit = {
 
     val ab = ArrayBuffer(1,2,3)
+//    println(ab)
+//    println(ab.mkString("\t"))
+//
+    ab(2)=22
+//
+//    println(ab)
+
+    val cc=ab(1)
+//    println(cc)
+
+    ab.append(44)
+
     println(ab)
-    println(ab.mkString("\t"))
+    val ac =ab:+33
+    println(ac)
   }
 }
