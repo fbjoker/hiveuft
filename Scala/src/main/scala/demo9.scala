bd1078eb012701d18c38af7929c44ff7175db6de
fbjoker <39256912@qq.com>
2018-11-22 11:46:10 +0800

    1122

--- Scala/src/main/scala/demo9.scala
+++ Scala/src/main/scala/demo9.scala
@@ -1,3 +1,35 @@
 object demo9 {
+  def main(args: Array[String]): Unit = {
 
+    val array = Array.ofDim[Int](10,10)
+
+    println(array.mkString(","))
+
+    for (elem <- array) {
+      println(elem.mkString("\t"))
+
+    }
+    println("=============================================")
+
+    array(1)(8)=18
+    for(x<-0 to 9 ;y<- 0 to 9){
+      array(x)(y)=x*y
+    }
+    for (elem <- array) {
+      println(elem.mkString("\t"))
+
+    }
+    println("=============================================")
+    for(x<-0 to 9 ;y<- 0 to 9){
+     if (x==y){
+
+      array(x)(y)=x*y
+     }
+    }
+    for (elem <- array) {
+      println(elem.mkString("\t"))
+
+    }
+
+  }
 }
