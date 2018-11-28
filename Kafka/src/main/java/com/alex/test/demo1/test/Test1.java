64669e8f2b29058cc0e17ca8095365af1dc07169
fbjoker <39256912@qq.com>
2018-11-28 08:04:50 +0800

    1128

---
+++ Kafka/src/main/java/com/alex/test/demo1/test/Test1.java
@@ -0,0 +1,15 @@
+package com.alex.test.demo1.test;
+
+import java.util.List;
+
+public class Test1 {
+    public static void main(String[] args) throws Exception {
+
+        List list = null;
+
+        for (Object obj : list) {
+            System.out.println(obj);
+        }
+
+    }
+}
